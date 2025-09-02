ab4976a80569ffb02c3b591775642da3f2be4cae
Liam-XD <liam.brotchie@xdesign.com>
2025-09-03 00:53:34 +0100

    included 'add item to cart' test

--- src/tests/steps/addToCartSteps.ts
+++ src/tests/steps/addToCartSteps.ts
@@ -1,13 +1,13 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
-import { page } from "../browserSetup";
-import { LoginPage } from "../page-objects/login-page.pom";
-import { InventoryPage } from "../page-objects/inventory-page.pom";
+import { page } from "../../../browserSetup";
+import { LoginPage } from "../../../page-objects/login-page.pom";
+import { InventoryPage } from "../../../page-objects/inventory-page.pom";
 
 let loginPage: LoginPage;
 let inventoryPage: InventoryPage;
 
-Given("I am on the product catalogue page", async () => {
+Given("I am on the product catalog page", async () => {
   loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.login();
